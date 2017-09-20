2f7dbcfb5e8001b7e89d17d7fb3288177bf1780f
kohyerim <kohyerim78@gmail.com>
2017-09-20 21:00:48 +0900

    test

--- Player.py
+++ Player.py
@@ -1,1 +1,52 @@
 import pygame
+vec = pygame.math.Vector2
+
+
+class Player :
+    def __init__(self, stage, width, height):
+        self.stage = stage
+        self.width = width
+        self.height = height
+        self.image = pygame.Surface((30, 30))
+        self.image.fill((0, 0, 255))
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.width/2, self.height/2)
+        self.pos = vec(0, self.height-50)
+        self.vel = vec(0, 0)
+        self.acc = vec(0, 0)
+        self.black = pygame.Surface((30, 30))
+        self.black.fill((0,0,0))
+
+    def jump(self):
+        self.vel.y = -10
+
+    def move(self):
+        self.acc = vec(0, 0.5)
+        pressed = pygame.key.get_pressed()
+        keydown = pygame.KEYDOWN
+        if keydown == pygame.K_UP:
+            self.jump()
+        if pressed[pygame.K_DOWN]:
+            pass
+        if pressed[pygame.K_RIGHT]:
+            self.acc.x = 0.35
+        if pressed[pygame.K_LEFT]:
+            self.acc.x = -0.35
+
+        self.acc.x += self.vel.x * (-0.08)
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+
+        self.rect.midbottom = self.pos
+
+        if self.pos.x > self.width:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = self.width
+            self.stage.blit(self.black, (0, self.pos.y))
+        if self.pos.y > self.height - 50:
+            self.pos.y = self.height - 80
+
+        self.stage.blit(self.image, (self.pos.x, self.pos.y))
+        self.stage.blit(self.black, (self.pos.x - 30, self.pos.y))
+        self.stage.blit(self.black, (self.pos.x + 30, self.pos.y))
--- Stage.py
+++ Stage.py
@@ -1,1 +1,47 @@
 import pygame
+from Player import Player
+
+class Stage:
+    width = 1200
+    height = 700
+    FPS = 30
+    fpsClock = pygame.time.Clock()
+
+    Stage = pygame.display.set_mode((width, height))
+
+    pygame.display.set_caption('M2Beat')
+    player = None
+
+    def __init__(self):
+        pygame.display.set_caption("M2Beat")
+        self.stage = pygame.display.set_mode((self.width, self.height))
+        self.stage.fill((0, 0, 0))
+        self.player = Player(self.stage, self.width, self.height)
+
+    class Ground:
+        def __init__(self, stage, x, y, w, h):
+            self.stage = stage
+            self.image = pygame.Surface((w, h))
+            self.image.fill((0, 255, 0))
+            self.rect = self.image.get_rect()
+            self.rect.x = x
+            self.rect.y = y
+            self.stage.blit(self.image, (self.rect.x, self.rect.y))
+
+
+    def start(self):
+        finish = False
+        while not finish :
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    finish = True
+            pygame.display.update()
+
+            self.Ground(self.stage, 0, self.height - 50, self.width, 50)
+            self.player.move()
+            pygame.display.update()
+
+
+
+game = Stage()
+game.start()
