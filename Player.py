d6699a93382b3a5cf0ab9c583dba7cee787d7e16
kohyerim <kohyerim78@gmail.com>
2017-09-22 01:47:41 +0900

    배경/바닥 이미지 추가

--- Player.py
+++ Player.py
@@ -4,9 +4,11 @@ PLAYER_ACC = 0.1 #가속
 PLAYER_FRICTION = -0.08 #마찰
 PLAYER_GRAV = 0.3 #중력
 
+pygame.display.set_mode((1200, 700))
+
 
 class Player:
-    def __init__(self, stage, width, height):
+    def __init__(self, stage, ground,  width, height):
         self.stage = stage
         self.walking = True #계속 움직이는 것처럼 보이게하는 상태
         self.jumping = False #점프 유무
@@ -16,34 +18,31 @@ class Player:
         self.height = height
         self.image = self.walk_frame[0]
         self.rect = self.image.get_rect()
-        self.rect.center = (self.width/2, self.height/2)
         self.pos = vec(100, self.height/2) #플레이어 시작 위치 (가로, 세로)
         self.vel = vec(0, 0) #속도계산
         self.acc = vec(0, 0) #가속계산
+        self.ground_height = ground.get_height()
 
     class Image: #이미지 불러오기
         def __init__(self, filename):
-            self.cut_image = pygame.image.load(filename)
+            self.cut_image = pygame.image.load(filename).convert_alpha()
 
         def get_image(self, x, y, w, h):
-            image = pygame.Surface((w, h))
-            image.blit(self.cut_image, (0, 0), (x, y, w, h))
+            image = pygame.Surface((w, h), pygame.SRCALPHA) #pygame.SRCALPHA -> png파일 배경 투명하게
+            image.blit(self.cut_image, (0, 0), (x, y, w, h)) #(x, y)좌표가 lefttop이고, (w, h)만한 크기로 자르기
             return image
 
-    slime = Image('resources/images/slime3.png') #슬라임이미지 불러오기
-    walk_frame = [slime.get_image(0, 0, 48, 48),
-                  slime.get_image(48, 0, 48, 48),
-                  slime.get_image(96, 0, 48, 48),
-                  slime.get_image(0, 48, 48, 48),
-                  slime.get_image(48, 48, 48, 48),
-                  slime.get_image(96, 48, 48, 48)] #걸어가는 모션(더 추가 가능)
+    slime = Image('resources/images/slime3Resized.png') #슬라임이미지 불러오기
+    walk_frame = [slime.get_image(0, 0, 96, 96),
+                  slime.get_image(96, 0, 96, 96),
+                  slime.get_image(192, 0, 96, 96),
+                  slime.get_image(0, 96, 96, 96),
+                  slime.get_image(96, 96, 96, 96),
+                  slime.get_image(192, 96, 96, 96)] #걸어가는 모션(더 추가 가능)
 
-    jump_frame = [slime.get_image(0, 96, 48, 48),
-                  slime.get_image(48, 96, 48, 48),
-                  slime.get_image(96, 96, 48, 48),
-                  slime.get_image(0, 144, 48, 48),
-                  slime.get_image(48, 144, 48, 48),
-                  slime.get_image(96, 144, 48, 48)] #점프할때 모션(더 추가 가능)
+    jump_frame = [slime.get_image(0, 192, 96, 96),
+                  slime.get_image(96, 192, 96, 96),
+                  slime.get_image(192, 192, 96, 96)] #점프할때 모션(더 추가 가능)
 
     def jump(self):
         self.vel.y = -10
@@ -72,20 +71,20 @@ class Player:
     def move(self): #플레이어 움직이는 함수
         self.animate()
         self.acc = vec(0, PLAYER_GRAV)
+        bottomlimit = self.height - self.ground_height - self.image.get_height() #(게임화면 세로길이) - (Ground 세로길이) - (이미지 세로길이)
         pressed = pygame.key.get_pressed()
         if pressed[pygame.K_RIGHT]:
-            self.pos.x += 1
+            self.pos.x += 2
         if pressed[pygame.K_LEFT]:
-            self.pos.x -= 1
+            self.pos.x -= 2
 
         self.acc.x += self.vel.x * PLAYER_FRICTION
         self.vel += self.acc
         self.pos += self.vel + PLAYER_GRAV * self.acc
 
-        if self.pos.y > self.height - 98: #바닥 밑으로 안떨어지게 하는 if문
-            self.pos.y = self.height - 98 #self.height - 98 = (self.height - 50)(Ground세로 길이) - 38(이미지 세로 크기)
+        if self.pos.y > bottomlimit + 1: #바닥(Ground) 밑으로 안떨어지게 하는 if문
+            self.pos.y = bottomlimit + 1
 
         self.rect.midbottom = self.pos
 
         self.stage.blit(self.image, (self.pos.x, self.pos.y))
-
--- Stage.py
+++ Stage.py
@@ -1,7 +1,7 @@
 import pygame
 from Player import Player
+#from time import sleep
 JUMPLIMIT = 2 #점프 한도
-
 
 class Stage:
     width = 1200 #가로
@@ -9,13 +9,15 @@ class Stage:
     FPS = 30
     fpsClock = pygame.time.Clock()
     player = None
+    background = pygame.image.load('resources/images/backgroundCutResized.png')
+    ground = pygame.image.load('resources/images/ground.png')
 
     def __init__(self):
         pygame.init()
         pygame.display.set_caption("M2Beat")
         self.stage = pygame.display.set_mode((self.width, self.height))
-        self.stage.fill((0, 0, 0)) #배경 색
-        self.player = Player(self.stage, self.width, self.height)
+        self.stage.blit(self.background, (0, 0)) #배경 색
+        self.player = Player(self.stage, self.ground, self.width, self.height)
         self.count = 0 #점프한 횟수(K_UP누른 횟수)
 
     def press_any_key(self):
@@ -33,24 +35,24 @@ class Stage:
         self.stage.blit(text_suface, text_rect)
 
     def start_screen(self): #시작화면
-        self.stage.fill((0, 0, 0))
+        self.stage.blit(self.background, (0, 0))
         self.draw_text("PRESS ANY KEY", 90, (255, 255, 255), self.width/2, self.height/2)
         pygame.display.update()
         self.press_any_key()
 
     class Ground: #플레이화면에서 땅(녹색)
-        def __init__(self, stage, x, y, w, h):
+        def __init__(self, stage, ground, x, y):
             self.stage = stage
-            self.ground_image = pygame.Surface((w, h))
-            self.ground_image.fill((0, 255, 0))
+            self.ground_image = ground
             self.x = x
             self.y = y
             self.stage.blit(self.ground_image, (self.x, self.y))
+            self.stage.blit(self.ground_image, (self.x + self.ground_image.get_width(), self.y))
 
     def update(self): #플레이어 움직일 때 잔상 안남게 + 플레이어 움직임
-        self.stage.fill((0, 0, 0))
+        self.stage.blit(self.background, (0, 0))
+        self.Ground(self.stage, self.ground, 0, self.height - self.ground.get_height())
         self.player.move()
-        self.Ground(self.stage, 0, self.height - 50, self.width, 50)
 
     def start(self):
         finish = False
@@ -63,10 +65,11 @@ class Stage:
                         self.player.jump()
                         self.count += 1
                     elif self.count == JUMPLIMIT:
-                        self.count = 0
+                        if self.player.pos.y == self.height - self.ground.get_height() - self.player.image.get_height() + 1:
+                            self.count = 0
+            self.player.move()
             self.update()
             pygame.display.update()
-
 
 game = Stage()
 game.start_screen()
