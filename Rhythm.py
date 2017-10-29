8f25d059d354c4afccfdd077743a1ba76a054036
lily486 <beayounha123@gmail.com>
2017-10-29 16:13:29 +0900

    수정함

--- Rhythm.py
+++ Rhythm.py
@@ -14,6 +14,11 @@ class Rhythm:
         self.stage = stage
         self.combo = 0
         self.combos = []
+        self.units = 0      # 총 맞춘 횟수
+        self.munit = 0      # 틀린 횟수
+        self.score = 0      # 콤보로 더해지는 점수
+        self.total = 0      # 총 점수
+
 
     def rhythm(self):
         pressed = pygame.key.get_pressed()
@@ -23,29 +28,51 @@ class Rhythm:
                 if pressed[pygame.K_UP]:
                     if pressed_key == UP:
                         self.combo += 1
+                        self.munit = 0          # 틀린 횟수 초기화
+                        self.units += 1         # 맞춘 횟수 +1
                         self.arrows.remove(obj)
                     else:
                         self.combo = 0
+                        self.munit += 1         # 틀린 횟수 +1
+                        self.units = self.units
                 elif pressed[pygame.K_DOWN]:
                     if pressed_key == DOWN:
                         self.combo += 1
+                        self.munit = 0
+                        self.units += 1
                         self.arrows.remove(obj)
                     else:
                         self.combo = 0
+                        self.munit += 1
+                        self.units = self.units
                 elif pressed[pygame.K_LEFT]:
                     if pressed_key == LEFT:
                         self.combo += 1
+                        self.munit = 0
+                        self.units += 1
                         self.arrows.remove(obj)
                     else:
                         self.combo = 0
+                        self.munit += 1
+                        self.units = self.units
                 elif pressed[pygame.K_RIGHT]:
                     if pressed_key == RIGHT:
                         self.combo += 1
+                        self.munit = 0
+                        self.units += 1
                         self.arrows.remove(obj)
                     else:
                         self.combo = 0
+                        self.munit += 1
+                        self.units = self.units
             elif obj.pos < 320:
                 self.combo = 0
+                self.units = self.units
 
     def ReturnCombo(self):
         return self.combo
+
+    def ReturnScore(self):
+        self.score = (100 * self.combo / 100)       # 콤보로 추가되는 점수
+        self.total = (100 * self.units) + (self.combo * self.score)     #맞춘횟수 백점단위 + 콤보로 추가되는 점수
+        return self.total
